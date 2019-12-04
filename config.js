a823c7dbbc29604e3515855a483565a3f900539a
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-04 18:30:47 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  consumer_key:         'tocomplete',
+  consumer_secret:      'tocomplete',
+  access_token:         'tocomplete',
+  access_token_secret:  'tocomplete',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
