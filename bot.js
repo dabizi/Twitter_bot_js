a0d0ae4b7a68f7921e09b64f1f28b17528e4004d
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-06 16:03:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,31 +6,42 @@ var config = require('./config');
 //console.log(config);
 var T = new Twit(config);
 
+tweetIt();
+// Interval for test : every 20 seconds
+//setInterval(tweetIt, 1000*20);
 
-var tweet = {
-  status: 'Hello World ! #gettingstarted'
-}
+// Interval for real, every 24h
+setInterval(tweetIt, 1000*60*60*24);
 
-T.post('statuses/update', tweet, tweeted);
+function tweetIt(){
 
-function tweeted(err, data, response){
-  if (err) {
-    console.log("Something went wrong!");
-  } else {
-    console.log("It worked!")
+  var r = Math.floor(Math.random()*100);
+
+  var tweet = {
+    status: 'Hola ' + r + ' #test'
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response){
+    if (err) {
+      console.log("Something went wrong!");
+    } else {
+      console.log("It worked!")
+    }
   }
 }
 
 /*
 var params = {
-  q: 'banana since:2011-11-11',
-  count: 100
+q: 'banana since:2011-11-11',
+count: 100
 };
 
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++)
-  console.log(tweets[i].text)
+var tweets = data.statuses;
+for (var i = 0; i < tweets.length; i++)
+console.log(tweets[i].text)
 };*/
